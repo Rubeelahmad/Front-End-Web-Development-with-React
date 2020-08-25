2fb8be90d0b6a21d6cc2b99efdb994e00c6489ab
Rubeelahmad <rubeelkhan45@gmail.com>
2020-08-26 01:36:40 +0500

    My First React App Testing Code

---
+++ ReactJS/new/src/Header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Component from 'react'
+
+class Header extends Component {
+    render() {
+      return(
+            <div>
+                <h1>
+                    This is Header 
+                </h1>
+            </div>
+      );
+    }
+  }
+
+export default Header
+
